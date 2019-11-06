864587b78dd3baccc8ab6505c3780a49e5f3014c
Bruno Ferreira <bruno.ferreira@climatempo.com.br>
2019-11-06 13:52:03 -0300

    refactor: Cleanup main App :recycle: :fire:

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import 'core-js/es/set';
 import 'core-js/stable/promise';
 import 'whatwg-fetch';
 
-import React, { useState, useEffect } from 'react';
-import CssBaseLine from "@material-ui/core/CssBaseline";
+import React, { useState } from 'react';
+import { CssBaseline } from "@material-ui/core";
 import 'typeface-roboto';
 import MainMenu from './MainMenu';
 import Map from './Map';
@@ -14,18 +14,12 @@ export default function App() {
   const height = window.innerHeight;
   const [map, setMap] = useState();
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(response => response.json())
-      .then(json => console.log(json));
-  }, []);
-
   const updateMap = newMap => setMap(newMap);
 
   return (
     <>
-      <CssBaseLine />
-      <MainMenu map={map} updateMap={updateMap} />
+      <CssBaseline />
+      <MainMenu map={map} />
       <Map height={height} mainMap={map} updateMap={updateMap} />
     </>
   );
